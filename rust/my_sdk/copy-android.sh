982fd695e3c6bd983c79a049840c7564a6dec6b9
ammarahm-ed <ammarahmed6506@gmail.com>
2023-09-17 23:16:36 +0500

    update some paths

--- rust/my_sdk/copy-android.sh
+++ rust/my_sdk/copy-android.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 COPY_DIR="../../packages/v8-module-cpp/src-native/android/src/main/jniLibs"
-LIB_NAME="libmy_sdk"
+NAME="my_sdk"
+LIB_NAME="lib$NAME"
 
 rm -rf $COPY_DIR
 
@@ -14,6 +15,8 @@ cp ./target/i686-linux-android/release/$LIB_NAME.so $COPY_DIR/x86/$LIB_NAME.so
 cp ./target/x86_64-linux-android/release/$LIB_NAME.so $COPY_DIR/x86_64/$LIB_NAME.so
 cp ./target/aarch64-linux-android/release/$LIB_NAME.so $COPY_DIR/arm64-v8a/$LIB_NAME.so
 cp ./target/armv7-linux-androideabi/release/$LIB_NAME.so $COPY_DIR/armeabi-v7a/$LIB_NAME.so
-cp -r ./include ../../packages/v8-module-cpp/src-native/android/src/main/cpp/my_sdk/include
+
+mkdir -p ../../packages/v8-module-cpp/src-native/android/src/main/cpp/$NAME
+cp -r ./include ../../packages/v8-module-cpp/src-native/android/src/main/cpp/$NAME
 
 echo "Dynamic libraries copied!"
--- rust/my_sdk/copy_ios.sh
+++ rust/my_sdk/copy_ios.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-LIB_NAME="MySdk"
+LIB_NAME="my_sdk"
+FRAMEWORK="MySdk"
 
-cp -r $LIB_NAME.xcframework ../../packages/v8-module-cpp/platforms/ios
-rm -rf $LIB_NAME.xcframework
-rm simulator_fat/libmy_sdk.dylib
+cp -r $FRAMEWORK.xcframework ../../packages/v8-module-cpp/platforms/ios
+rm -rf $FRAMEWORK.xcframework
+rm simulator_fat/lib$LIB_NAME.dylib
