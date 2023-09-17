3baacb314569ab8b3aa9e4c651b3fb7eb54f5585
ammarahm-ed <ammarahmed6506@gmail.com>
2023-09-17 23:14:07 +0500

    some updates

--- rust/my_sdk/copy_ios.sh
+++ rust/my_sdk/copy_ios.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 LIB_NAME="MySdk"
 
-cp -r $LIB_NAME.xcframework ../../packages/v8-module-cpp/src-native/ios
-cp $LIB_NAME.podspec ../../packages/v8-module-cpp/src-native/ios
+cp -r $LIB_NAME.xcframework ../../packages/v8-module-cpp/platforms/ios
 rm -rf $LIB_NAME.xcframework
-rm simulator_fat/libmy_sdk.a
+rm simulator_fat/libmy_sdk.dylib
